75485645b3d94916a847b798ffdf9655bc5b352e
unknown <gilangramdhani2001@gmail.com>
2022-11-03 10:38:47 +0800

    File Project Baru 03/11/22

--- pertemuan 2/index.php
+++ pertemuan 2/index.php
@@ -1,3 +1,29 @@
 <?php 
-echo "yufytfty";
+//standar output di php
+//echo, print, print-f, var_dump
+
+$nama = "M.Gilang Ramdahni";
+echo "Hello, Nama saya $nama";
+
+//Operator
+//aritmatika
+//* / + - %
+$x = 10;
+$y = 20;
+echo $x + $y;
+
+//penggabungan string /concatenation /concat//.
+$nama_depan = "M.Gilang";
+$nama_belakang = "ramdhani";
+echo $nama_depan . " " . $nama_belakang
+
+//assigment
+//=,+=,-=,/=,%=,.=
+$x = 1;
+$x /= 5;
+echo $x;
+$nama .= $nama_depan;
+$nama .= " ";
+$nama_belakang;
+echo $nama;
 ?>
