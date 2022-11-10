a3deabbc680e97df4460082d8019a57b222351bf
unknown <gilangramdhani2001@gmail.com>
2022-11-10 10:44:05 +0800

    kuliah/10/11/22

---
+++ pertemuan 3/Latihan1.php
@@ -0,0 +1,30 @@
+<?php
+// perulangan looping
+// for, while, do.. while
+// foreach : pengulangan khusus array
+
+// Menggunakan for
+for($i = 0; $i < 5; $i++){
+ echo "Hello World! <br>";
+}
+
+// menggunakan while
+// $i = 0;
+// while($i < 5) {
+//   echo "Hello World! <br>";
+// $i++;
+//}
+
+// menggunakan do.. while
+// $i = 0;
+// do {
+//   echo "Hello World! <br>";
+// $i++;
+//}while($i < 5);
+
+si = 3;
+do {
+
+}
+
+?>
---
+++ pertemuan 3/Latihan2.php
@@ -0,0 +1,16 @@
+<?php
+  // Kondisi atau percabangan
+  // if else
+  // if else if else
+  // ternary
+  // switch
+
+  // if
+  $x = 10;
+  if( $x < 20 ) {
+      echo "Benar";
+  }
+  echo "Salah";
+
+  
+?>
